e30b7bb3d7d1210435aee500fd3c4c17b91af852
cdaly33 <dalyco884@gmail.com>
2017-08-08 16:22:41 -0700

    fixed cases with 0.00 again

--- scrolling-sign-python-client/main.py
+++ scrolling-sign-python-client/main.py
@@ -52,8 +52,10 @@ def gainstring(k, pair):
 		return " "
 	delta = float(k.getTickerInfo(pair)['result'][cryptocode[pair]]['a'][0]) - float(cryptocache[pair])
 	if (round(delta, 2)>=0):
-		if (str(round(delta, 2))[0]=='-'):
-			return " +" + str(round(delta, 2))[1:]+" "
+		if (round(delta, 2))==0):
+			if (str(round(delta, 2))[0]=='-'):
+				return " +" + str(round(delta, 2))[1:]+" "
+			return " +" + str(round(delta, 2))+" "
 		return "[+" + str(round(delta, 2))+"]"
 	else:
 		return "{-" + str(round(delta, 2))[1:]+"}"
