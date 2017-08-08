ad3d8ecd08972363a31ab2cc98a6d9437bd5b433
cdaly33 <dalyco884@gmail.com>
2017-08-08 13:59:39 -0700

    too many reps

--- scrolling-sign-python-client/main.py
+++ scrolling-sign-python-client/main.py
@@ -120,10 +120,10 @@ if __name__ == "__main__":
 					showtext = showtext + pricestring(k, 'XBTUSD')
 					showtext = showtext + pricestring(k, 'LTCUSD')
 					showtext = showtext + pricestring(k, 'ETHXBT')
-					repetitions = 7
+					repetitions = 6
 				else:
 					showtext = pricestring(k, pair)
-					repetitions = 8
+					repetitions = 7
 				updatecache(k, 'ETHUSD')
 				updatecache(k, 'XBTUSD')
 				updatecache(k, 'LTCUSD')
